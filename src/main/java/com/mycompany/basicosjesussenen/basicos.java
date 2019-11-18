fe64216076a844d25f59884464bda9da1bb0f920
Etevago <Etevago@DESKTOP-Q0RCHC4>
2019-11-18 18:20:04 +0100

    numericos

--- src/main/java/com/mycompany/basicosjesussenen/basicos.java
+++ src/main/java/com/mycompany/basicosjesussenen/basicos.java
@@ -12,5 +12,23 @@ package com.mycompany.basicosjesussenen;
  */
 public class basicos {
 
+public static void numericos(){
+byte elMasPeque;
+short chiquitin;
+int entero;
+long duplicaEntero;
+float grande;
+double doble;
 
+entero=10;
+doble=5.6;
+System.err.println("Valor entero");
+System.out.println(entero);
+System.err.println("Valor doble");
+System.out.println(doble);
+}
+
+public static void main (Sring args[]){
+numericos();
+}
 }
